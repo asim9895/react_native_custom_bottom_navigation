e5e3bb62ed1cf105699bd63cf020357b5ed048a4
asim9895 <75245102+asim9895@users.noreply.github.com>
2021-06-27 12:59:12 +0530

    added bottom navigation

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-import { NavigationContainer } from '@react-navigation/native';
+ 
 import Tabs from './navigation/Tabs';
 
 export default function App() {
